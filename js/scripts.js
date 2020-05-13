5b285560fbc9ae238bbcce431d61510c635c8eb8
Samantha Butters <61861742+snbutters@users.noreply.github.com>
2020-05-12 21:47:00 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// Business Logic (back-end)
+
+const leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// jQuery User Interface Logic
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    const year = parseInt($("input#year").val());
+    const result = leapYear(year);
+    $("#result").text(result);
+  });
+});
