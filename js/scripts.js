a25615e24826abfe566f46718cf998d1bce44972
Samantha Butters <61861742+snbutters@users.noreply.github.com>
2020-05-12 22:01:33 -0700

    update output section of the page.

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,15 @@ $(document).ready(function() {
     event.preventDefault();
     const year = parseInt($("input#year").val());
     const result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
